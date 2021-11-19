cd24719d3995051d5e2d7f9c0b13dbd7a576af35
Katharine Hao <hao.katharine@gmail.com>
2021-11-19 11:21:50 +0200

    moved frontend inside repo

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,10 @@ import express from 'express'
 import lusca from 'lusca'
 import dotenv from 'dotenv'
 import compression from 'compression'
+import cors from 'cors'
+import passport from 'passport'
 
+import { googleStrategy } from './config/passport'
 import bookRouter from './routers/book'
 import userRouter from './routers/user'
 import apiErrorHandler from './middlewares/apiErrorHandler'
@@ -16,12 +19,16 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 app.use(apiContentType)
 // Use common 3rd-party middlewares
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(compression())
 app.use(express.json())
 app.use(lusca.xframe('SAMEORIGIN'))
 app.use(lusca.xssProtection(true))
+app.use(cors())
 
 // Use movie router
+passport.use(googleStrategy)
 app.use('/api/v1/books', bookRouter)
 app.use('/api/v1/users', userRouter)
 //filtering
