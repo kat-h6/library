bf8706fd9b18ea9e622b5145383406dc3153c69f
Katharine Hao <hao.katharine@gmail.com>
2022-01-10 09:12:22 +0200

    cleaning up

--- client/src/Routes.tsx
+++ client/src/Routes.tsx
@@ -5,7 +5,6 @@ import { Routes as Switch, Route } from 'react-router-dom' // updated to latest
 //https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
 
 import Home from './pages/Home'
-import Product from './pages/Product'
 import BookDetails from './pages/BookDetails'
 import Search from './pages/Search'
 import Dashboard from './pages/Dashboard'
@@ -16,7 +15,6 @@ const Routes = () => (
   <Switch>
     <Route path="/" element={<Home />} />
     <Route path="/books/:bookId" element={<BookDetails />} />
-    <Route path="/products/:id" element={<Product />} />
     <Route path="/books" element={<Books />} />
     <Route path="/books/new" element={<NewBook />} />
     <Route path="/books/search" element={<Search />} />
